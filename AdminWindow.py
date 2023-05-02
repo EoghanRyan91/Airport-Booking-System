af84cb9f61db8bdc0225d2afdf6e35a6bc518cf5
Eoghan Ryan <70537420+EoghanRyan91@users.noreply.github.com>
2023-05-02 23:11:21 +0100

    Update AdminWindow.py

--- AdminWindow.py
+++ AdminWindow.py
@@ -1,13 +1,19 @@
 import tkinter as tk
+from tkinter import ttk
+import sqlite3
 
 class AdminWindow:
     def __init__(self):
         self.window = tk.Tk()
-        self.window.geometry("300x300")
+        self.window.geometry("600x300")
         self.window.title("Admin Window")
 
         self.label = tk.Label(self.window, text="This is the Admin Window")
-        self.label.pack()
+        
+
+        db_button = ttk.Button(self.window, text="Display DB Contents")
+        db_button.grid(row=1, column=1, padx=5, pady=5)
+      
 
         self.window.mainloop()
 
